5a80fc4021def19aa59e556ab860a3aff7cc8134
shereen <shereen.hamato@gmail.com>
2025-01-23 07:57:06 +0800

    update employee service

--- src/main/kotlin/com/employee/employee/service/EmployeeService.kt
+++ src/main/kotlin/com/employee/employee/service/EmployeeService.kt
@@ -4,8 +4,33 @@ import com.employee.employee.repository.EmployeeRepository
 class EmployeeService(private val employeeRepository: EmployeeRepository){
     fun getAllEmployee():List<Employee> = employeeRepository.findAll()
 
-    fun getEmployeeById(id:Long):Employee = employeeRepository.findById(id)
-    .orElseThrow{RuntimeException("Employee not found")}
+    fun getEmployeeById(id:Long):Employee {
+        return employeeRepository.findById(id)
+            .orElseThrow { RuntimeException("Employee not found") }
+    }
+
+    fun createEmployee(employee:Employee):Employee = employeeRepository.save(employee)
+
+    fun updateEmployee(id:Long, employee:Employee):Employee {
+            return  if(employeeRepository.existsById(id)){
+                employeeRepository.save(
+                    Employee(
+                        id= employee.id,
+                        name= employee.name,
+                        surname = employee.surname,
+                        email= employee.email,
+                        department = employee.department,
+                        jobTitle = employee.jobTitle
+                    )
+                )
+            } else throw RuntimeException("Employee not found")
+    }
+
+    fun deleteEmployee(id:Long){
+        if(employeeRepository.existsById(id)){
+            employeeRepository.deleteById(id)
+        }else throw RuntimeException("Employee not found")
+    }
 
 
 
